d1a1f127d5fc07264a6d3b30494f7a350fd24224
JohnGardiner93 <john.gardiner93@gmail.com>
2021-09-24 06:08:55 -0400

    Server setup
    Adds server.js code with required dependencies installed (mongoose,
    dotenv, express). Config information on config.env.

--- server.js
+++ server.js
@@ -1,1 +1,35 @@
-// mongodb+srv://johng:<password>@cluster0.bhrz8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+////////////////////////////////////////////
+// MODULES
+const express = require('express');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const app = express();
+
+////////////////////////////////////////////
+// SETUP
+// Environment Variables
+dotenv.config(); //Read .env
+dotenv.config({ path: './config.env' });
+
+// Server Config
+const port = process.env.PORT || 3000;
+
+// DB Setup
+const DB = process.env.DB_STRING.replace(`{%USER%}`, process.env.USER).replace(
+  '{%PASSWORD%}',
+  process.env.DATABASE_PASSWORD
+);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    // useCreateIndex: true,
+    // useFindandModify: false,
+  })
+  .then(console.log('I did it'));
+
+////////////////////////////////////////////
+// SERVER
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
