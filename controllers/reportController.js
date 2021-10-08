59afb5252afc4dfdf0c4427e1a4f27dbf9be519b
JohnGardiner93 <john.gardiner93@gmail.com>
2021-10-08 06:12:39 -0400

    Create Report Method
    Adds create report method. Successfully adds report and rejects
    duplicate reports.

--- controllers/reportController.js
+++ controllers/reportController.js
@@ -12,11 +12,23 @@ exports.getAllReports = async (req, res, next) => {
   });
 };
 
-exports.createReport = async (req, res, next) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined (createReport)',
-  });
+exports.createReport = async (req, res) => {
+  try {
+    // console.log(req.body);
+    const newReport = await Report.create(req.body);
+
+    res.status(201).json({
+      status: 'success',
+      data: {
+        report: newReport,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: 'error',
+      message: `Report entry creation failed: ${error.message}`,
+    });
+  }
 };
 
 exports.getReport = async (req, res, next) => {
