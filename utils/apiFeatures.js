353c92a9e31edd142be6969670f45a3d6d06df30
JohnGardiner93 <john.gardiner93@gmail.com>
2021-11-10 04:49:39 -0500

    APIFeatures: Array Handling
    Adjusted APIFeatures.js such that it can handle query strings with
    arrays.

--- utils/apiFeatures.js
+++ utils/apiFeatures.js
@@ -9,19 +9,20 @@ class APIFeatures {
   filter() {
     const excludedFields = ['page', 'sort', 'limit', 'fields']; // Won't be directly used in DB query, but used to filter and sort the data sent by the user.
 
+    // Create Query Object
     const queryObj = { ...this.queryString }; // Shallow copy of query String
-    // console.log(`The query object is: ${JSON.stringify(queryObj)}`);
 
+    // Modify Query Object
     excludedFields.forEach((el) => delete queryObj[el]); //deletes properties
+    const modObj = this._splitObjectStringsToArrays(queryObj);
 
-    let queryStr = JSON.stringify(queryObj);
+    // Create & Modify Query String from QUery Object
+    let queryStr = JSON.stringify(modObj);
     queryStr = queryStr.replace(
-      /\b(gte|gt|lte|lt|ne)\b/g,
+      /\b(gte|gt|lte|lt|ne|nin|in)\b/g,
       (match) => `$${match}`
     );
-    // .replace(/,/g, '');
 
-    // console.log(queryStr);
     this.query = this.query.find(JSON.parse(queryStr)); // replace input query string with the modified one. We remove page, sort, etc. so that we can interpret human-readable query and parse it into Mongo's preferred format (using methods within this class)
 
     return this;
@@ -56,6 +57,26 @@ class APIFeatures {
     this.query = this.query.skip(skip).limit(limit);
 
     return this;
+  }
+
+  /**
+   * Recursively burrows into an object. String values are split into arrays based on the provided split character. Mutates properties of provided target (if they qualify). Returns unmodified target if target is not object.
+   * @param {Object} target - Target object
+   * @param {String} splitChar - Sub string with which to split the object's strings
+   * @returns {Object} - Target object (mutated if strings were present)
+   */
+  _splitObjectStringsToArrays(target, splitChar = ',') {
+    if (typeof target !== 'object') return target;
+
+    for (const key in target) {
+      if (typeof target[key] === 'object') {
+        this._splitObjectStringsToArrays(target[key]);
+      }
+      if (typeof target[key] === 'string') {
+        target[key] = target[key].split(splitChar);
+      }
+    }
+    return target;
   }
 }
 
