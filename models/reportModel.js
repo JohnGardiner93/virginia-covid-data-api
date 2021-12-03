f22eda5481746bf21f4831cc9a7ac04eae479231
JohnGardiner93 <john.gardiner93@gmail.com>
2021-12-03 05:26:56 -0500

    Add virtual properties to reportModel
    Adds two more virtual properties to reportModel:
    caseHospitalizationRatio and hospitalizationFatalityRatio.
    
    Adds "toJSON" and "toObject" options to schema so that virtual
    properties are captured during conversion of document to JSON or Object.

--- models/reportModel.js
+++ models/reportModel.js
@@ -2,51 +2,68 @@
 // Modules
 const mongoose = require('mongoose');
 
-const reportSchema = new mongoose.Schema({
-  reportDate: {
-    type: Date,
-    required: [true, 'A report must have a reportDate'],
-  },
-  region: {
-    type: String,
-    required: [true, 'A report must have a region'],
-    trim: true,
-  },
-  ageGroup: {
-    type: String,
-    required: [true, 'A report must have a ageGroup'],
-    enum: {
-      values: [
-        '0-9 Years',
-        '10-19 Years',
-        '20-29 Years',
-        '30-39 Years',
-        '40-49 Years',
-        '50-59 Years',
-        '60-69 Years',
-        '70-79 Years',
-        '80+ Years',
-        'Missing',
-      ],
-      message: 'Not a valid age group. See documentation.',
+////////////////////////////////////////////
+const reportSchema = new mongoose.Schema(
+  {
+    reportDate: {
+      type: Date,
+      required: [true, 'A report must have a reportDate'],
+    },
+    region: {
+      type: String,
+      required: [true, 'A report must have a region'],
+      trim: true,
+    },
+    ageGroup: {
+      type: String,
+      required: [true, 'A report must have a ageGroup'],
+      enum: {
+        values: [
+          '0-9 Years',
+          '10-19 Years',
+          '20-29 Years',
+          '30-39 Years',
+          '40-49 Years',
+          '50-59 Years',
+          '60-69 Years',
+          '70-79 Years',
+          '80+ Years',
+          'Missing',
+        ],
+        message: 'Not a valid age group. See documentation.',
+      },
+    },
+    numberOfCases: {
+      type: Number,
+      required: [true, 'A report must have a numberOfCases'],
+    },
+    numberOfHospitalizations: {
+      type: Number,
+      required: [true, 'A report must have a numberOfHospitalizations'],
+    },
+    numberOfDeaths: {
+      type: Number,
+      required: [true, 'A report must have a numberOfDeaths'],
     },
   },
-  numberOfCases: {
-    type: Number,
-    required: [true, 'A report must have a numberOfCases'],
-  },
-  numberOfHospitalizations: {
-    type: Number,
-    required: [true, 'A report must have a numberOfHospitalizations'],
-  },
-  numberOfDeaths: {
-    type: Number,
-    required: [true, 'A report must have a numberOfDeaths'],
-  },
-});
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
 
 reportSchema.virtual('caseFatalityRatio').get(function () {
   const ratio = this.numberOfCases / this.numberOfDeaths;
+  return Number(ratio.toPrecision(1));
+});
+
+reportSchema.virtual('caseHospitalizationRatio').get(function () {
+  const ratio = this.numberOfCases / this.numberOfHospitalizations;
+  return Number(ratio.toPrecision(1));
+});
+
+reportSchema.virtual('hospitalizationFatalityRatio').get(function () {
+  const ratio = this.numberOfHospitalizations / this.numberOfDeaths;
   return Number(ratio.toPrecision(1));
 });
 
